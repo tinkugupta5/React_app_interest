9f33db3a075499512719b666f627db8d5ee0f0d4
tinku gupta <tinku.gupta4@gmail.com>
2022-10-31 23:29:41 +0530

    syntax changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Button from '@mui/material/Button';
 import {useState} from 'react'
 
 function App() {
-
   const [num1,setNum1] = useState(0);
   const [num2,setNum2] = useState(0);
   const [num3,setNum3] = useState(0);
